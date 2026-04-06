82da9718f87f8c3dc703e4a29322a4a3343f6aa1
MichaelCodes <michael.lang3303@gmail.com>
2026-04-05 19:10:57 -0500

    March 5th

---
+++ 6kyu/genetic-algorithm-series-5-roulette-wheel.js
@@ -0,0 +1,32 @@
+// The "Roulette wheel selection", also known as "Fitness proportionate selection", is a genetic operator used in genetic algorithms for selecting potentially useful solutions for recombination.
+
+// Your task is to implement it.
+
+// roulette
+
+// You throw a coin in and has a chance to fall in one of the slots, the higher the fitness the higher the chance the element has to be selected.
+
+// Given the population and fitnesses, your task is to run the roulette to return one element.
+
+// Note: Some tests are random. If you think your algorithm is correct but the result fails, trying again should work.
+
+//My Solution
+const select = (population, fitnesses) => {
+  let num = Math.random();
+  let output;
+  let rangeStart;
+  let rangeEnd;
+  fitnesses.forEach((e,i)=>{
+    if(i===0){
+      rangeStart = 0;
+      rangeEnd = e;
+    }else{
+      rangeStart = rangeEnd
+      rangeEnd = rangeEnd+e
+    }
+    if(num>rangeStart && num<rangeEnd){
+      output = i
+    }
+  })
+  return population[output]
+};
